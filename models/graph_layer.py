d9d2a2aac424724cdbae63df11064257f7721dc5
Ayomide Oguntuase <oguntuase3@gmail.com>
2026-02-25 20:58:35 +0100

    permute edge index

--- models/graph_layer.py
+++ models/graph_layer.py
@@ -68,7 +68,7 @@ class GraphLayer(MessagePassing):
         print('edge_index', edge_index.shape)
         print('embedding', embedding.shape)
 
-        out = self.propagate(edge_index, x=x, embedding=embedding, edges=edge_index,
+        out = self.propagate(edge_index, x=x, embedding=embedding, edges=edge_index.permute(1, 0, 2),
                              return_attention_weights=return_attention_weights)
 
         if self.concat:
